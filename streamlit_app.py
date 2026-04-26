d67a30aa476d1b998afaf88fad3d6c56c369fc8d
Adam Bayraktar <abayraktar@edu.cdv.pl>
2026-04-26 12:24:08 +0200

    fix typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import os
 st.set_page_config(layout="wide", page_title="Gemini chatbot app")
 st.title("Gemini chatbot app")
 
-api_key, base_url = st.secrects["API_KEY"], st.secrects["BASE_URL"]
+api_key, base_url = st.secrets["API_KEY"], st.secrets["BASE_URL"]
 selected_model = "gemini-2.5-flash"
 
 if "messages" not in st.session_state:
