d9e56f15d18881a6ed03f72e2c4377099944b671
Adam Bayraktar <adambayraktar@gmail.com>
2026-06-12 23:54:39 +0200

    add pdf loader

--- docloader.py
+++ docloader.py
@@ -1,5 +1,18 @@
-import os 
-import fitz
+import os
+import pymupdf
 
+def load_pdf(file_path):
+    doc = pymupdf.open(file_path)
+    text = ""
+    for page in doc:
+        text += page.get_text()
+    doc.close()
+    return text
 
-def load_pdf():
+def load_documents_from_folder(folder_path):
+    documents = []
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".pdf"):
+            text = load_pdf(os.path.join(folder_path, filename))
+            documents.append({"filename": filename, "text": text})
+    return documents
