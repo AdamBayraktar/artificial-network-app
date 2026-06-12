69dab4d191c0c83e87b8cd5335e14a9704a36d14
Adam Bayraktar <adambayraktar@gmail.com>
2026-06-12 23:21:20 +0200

    test only assistant

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?."}]
 
 for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
+    st.chat_message("assistant").write(msg["content"])
 
 if prompt := st.chat_input("Say something and/or attach an image",
     accept_file=True,
